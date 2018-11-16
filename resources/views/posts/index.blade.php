7c6e29d45a044a180448d6205ba792729cbecb3a
chunkai <chunkai@dches.chc.edu.tw>
2018-11-16 08:53:55 +0800

    製作表單

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-12">
                 <h2>最新公告</h2>
-                <a href="{{ route('posts.create') }}" class="btn btn-success">新增公告</a>
+                <a href="{{ route('posts.create') }}" class="btn btn-success btn-sm">新增公告</a>
             </div>
         </div>
     </div>
