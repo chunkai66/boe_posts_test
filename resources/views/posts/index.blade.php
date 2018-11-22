73af0adc4baf8a4d4bca5ae4da8035f5a6391041
chunkai <chunkai@dches.chc.edu.tw>
2018-11-22 11:48:03 +0800

    php artisan make:policy PostPolicy --model=Post 針對新增、修改、刪除 分別設定權限政策

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,11 +5,9 @@
         <div class="row">
             <div class="col-12">
                 <h2>最新公告</h2>
-                @auth
-                    @if(auth()->user()->admin==1)
-                        <a href="{{ route('posts.create') }}" class="btn btn-success">新增公告</a>
-                    @endif
-                @endauth
+                @can('create',App\Post::class)
+                    <a href="{{ route('posts.create') }}" class="btn btn-success">新增公告</a>
+                @endcan
             </div>
             <div class="col-12">
                 <table class="table table-hover">
