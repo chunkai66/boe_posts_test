ab116dca60ce4b0cde432686b4d2f23392bc7007
chunkai <chunkai@dches.chc.edu.tw>
2018-11-22 21:03:35 +0800

    自訂函式 自訂config

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h2>最新公告</h2>
+                <h2>{{ config('boe.test') }}最新公告{{ hello_world() }}</h2>
                 @can('create',App\Post::class)
                     <a href="{{ route('posts.create') }}" class="btn btn-success">新增公告</a>
                 @endcan
